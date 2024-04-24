f7e537ad7aae379e177ac80a064ebc9e4ee67892
Santiago Franco <ktns0930@gmail.com>
2024-04-23 21:07:26 -0500

    test sonarcloud

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.post("/product", async (req, res) => {
   }
 });
 
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Node JS is running on port 3000");
 });
